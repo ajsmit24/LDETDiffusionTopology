cgValidation calculation now working for diffusion coef - I have determined I also need mean first passage times... I had to implement bug fixes and issues with lattice node types in other classes

common/convergence_criteria.py
@@ -19,7 +19,7 @@ log=mylogger.log
 
 
 class Diffusion_Conv():
-    def __init__(self,result_writer,graph_dim,threshold_type="relative_std",threshold_limit=0.05):
+    def __init__(self,run_id,result_writer,graph_dim,threshold_type="relative_std",threshold_limit=0.05):
         self.threshold_types=["relative_std","absolute_value"]
         if(threshold_type not in self.threshold_types):
             raise Exception("ERROR: Invalid argument value threshold_type="+threshold_type)
@@ -27,39 +27,107 @@ class Diffusion_Conv():
         self.threshold_limit=threshold_limit
         self.graph_dim=graph_dim
         self.result_writer=result_writer
+        self.run_id=run_id
         
         self.conv_check_count=0
         
         self.running_stats={"<x^2>":Welford(),"D":Welford()}
+        #for i in range(self.graph_dim):
+        #    self.running_stats[str(i)]=Welford()
         
     def calc_mean_and_var(self,new_data,stat_type):
         for nd in new_data:
             self.running_stats[stat_type].add(np.array(nd))
-        return self.running_stats[stat_type].mean, self.running_stats[stat_type].var_s
+        return self.running_stats[stat_type].mean.item(), self.running_stats[stat_type].var_s.item()
     def update_diffusion_coef(self,time,distance):
         #msd=mean squared displacement
         msd_x2,std_x2=self.calc_mean_and_var([distance],"<x^2>")
-        mean_D,std_D=self.calc_mean_and_var([msd_x2/((2**self.graph_dim)*time)],"D")
-        return {"<x^2>":{"mean":msd_x2,"std":std_x2},"D":{"mean":mean_D,"std":std_D}}
+        D=msd_x2/((2**self.graph_dim)*time)
+        mean_D,std_D=self.calc_mean_and_var([D],"D")
+        return {"D_cur":D,"<x^2>":{"mean":msd_x2,"std":std_x2},"D":{"mean":mean_D,"std":std_D}}
         
     def check_conv(self,rand_walk):
-        stats=self.update_diffusion_coef(rand_walk.total_steps,rand_walk.distance)
-        if(self.threshold_types=="relative_std"):
-            x2_good=stats["<x^2>"]["std"]/stats["<x^2>"]["mean"]<self.threshold_limit
-            D_good=stats["D"]["std"]/stats["D"]["mean"]<self.threshold_limit
+        stats=self.update_diffusion_coef(rand_walk.total_steps,rand_walk.distance**2)
+        if(self.threshold_type=="relative_std"):
+            #x2_rel_std=stats["<x^2>"]["std"]/stats["<x^2>"]["mean"]
+            #x2_good=x2_rel_std<self.threshold_limit
+            D_rel_std=stats["D"]["std"]/stats["D"]["mean"]
+            D_good=D_rel_std<self.threshold_limit
+            #isconv=x2_good and D_good
+            isconv=D_good
+            #pos_mean=[]
+            #for i in range(self.graph_dim):
+                #m,_=self.calc_mean_and_var([rand_walk.particle_pos_pbc_corrected[i]],str(i))
+                #pos_mean.append(m)
             self.result_writer.write({"time":rand_walk.total_steps,
                                  "distance":rand_walk.distance,
-                                 "D":stats["D"]["mean"],
-                                 "<x^2>":stats["<x^2>"]["mean"],
-                                 "STD":{"<x^2>":stats["<x^2>"]["std"],"D":stats["D"]["std"]},
-                                 "position":self.particle_pos_pbc_corrected
+                                 "D":stats["D_cur"],
+                                 "D_mean":stats["D"]["mean"],
+                                 #"<x^2>":stats["<x^2>"]["mean"],
+                                 #"x2_red_std":x2_rel_std,
+                                 "D_rel_std":D_rel_std,
+                                 "STD":{
+                                     #"<x^2>":stats["<x^2>"]["std"],
+                                     "D":stats["D"]["std"]},
+                                 "position":[rand_walk.particle_pos_pbc_corrected,rand_walk.particle_location],
+                                 #"pomo_pbc":[rand_walk.particle_pos_pbc_corrected[i]%rand_walk.options["latticeObj"].dimensions[i] for i in range(self.graph_dim)],
+                                 #"pomo_raw":[rand_walk.particle_location[i]%rand_walk.options["latticeObj"].dimensions[i] for i in range(self.graph_dim)],
+                                 "run_id":self.run_id,
+                                 "isconv":isconv,
+                                 #"pos_mean":pos_mean
                                  })
-            return x2_good and D_good
+            return isconv
         else:
-            raise Exception("ERROR:threshold_types!=relative_std has not been implemented")
+            raise Exception("ERROR:threshold_types!=relative_std has not been implemented. Specified value:"+self.threshold_type)
 
 
 
+#The way I want this to work in accordance with the pilot is essentially as follows
+#the pilot will spawn a "run" this run then spawns a dimension d "subrun" and a
+#dimension d-1 "subrun" - eahc of these subruns are further decmoposed into
+#individual calculations of the diffusion coefficent - these are differentialable by their
+#runid and dimension - each of these individual claculations are run separately via MPI 
+#but calculation of the smae dimension are written to the same file
+#then convergence is checked for each dimension
+#if convergence is not reached a the process restarts with a new batch number 
+#note that run_ids must be continuos accross batches and can not reset
+#since they define the random seed
+#additionally different batches will be writen to different files to keep files
+#small and minimize chances for additional strange behavior with MPI writes
+class Overall_Diffusion_Conv():
+    def __init__(self,threshold=0.05):
+        self.threshold=threshold
+        #self.running_stats={"<x^2>":Welford(),"D":Welford(),"t":Welford()}
+        self.running_stats={"D":Welford()}
+        
+    def check_conv(self,output_files,verbose=True):
+        for of in output_files:
+            reader=utils.ResultReader(of)
+            #getting these final states may be slightly... faster in reverse
+            #or maybe the writer should output something when a final state is reached
+            for line in reader.read():
+                if(line["isconv"]):
+                    for k in self.running_stats:
+                        self.running_stats[k].add(np.array(line[k]))
+            
+            is_all_conv=True         
+            rel_stds={}
+            means={}
+            abs_std={}
+            for k in self.running_stats:
+                means[k]=self.running_stats[k].mean.item()
+                abs_std[k]=self.running_stats[k].var_s.item()
+                rel_stds[k]=abs_std[k]/means[k]
+                is_all_conv=is_all_conv and rel_stds[k]<self.threshold
+                
+            if(not verbose):
+                return is_all_conv
+            else:
+                return is_all_conv,rel_stds
+                #return is_all_conv,{"rel_stds":rel_stds,"means":means,"abs_std":abs_std}
+                    
+            
+        
 
 #rolling averages convergence in ratio of two things
 def old_ratio_conv(past,fn,conv_thresh=0.001):